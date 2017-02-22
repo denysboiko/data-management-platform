5ec9e32ff839aec64e42217362eb8208a3da26a0
denysboiko <denys.bojko@gmail.com>
2017-02-22 17:10:43 +0200

    moving to django

--- reach_ukraine/settings.py
+++ reach_ukraine/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'portal'
 ]
 
 MIDDLEWARE = [
@@ -114,8 +116,10 @@ USE_L10N = True
 
 USE_TZ = True
 
+AUTH_USER_MODEL = 'portal.User'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'portal', 'portal/static')
--- reach_ukraine/urls.py
+++ reach_ukraine/urls.py
@@ -1,3 +1,5 @@
+from portal.views import *
+
 """reach_ukraine URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -13,9 +15,14 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+
 from django.conf.urls import url
 from django.contrib import admin
 
+
 urlpatterns = [
+    url(r'^$', main),
+    url(r'^upload/', upload),
+    url(r'^draggable/', draggable),
     url(r'^admin/', admin.site.urls),
 ]
